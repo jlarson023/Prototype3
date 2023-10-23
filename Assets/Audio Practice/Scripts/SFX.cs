672e595880d7131bae95f491255b1842bc8c539d
jlarson023 <larsontjonathan@gmail.com>
2023-10-23 09:38:39 -0700

    3.4 Done

--- Assets/Audio Practice/Scripts/SFX.cs
+++ Assets/Audio Practice/Scripts/SFX.cs
@@ -16,7 +16,7 @@ public class SFX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Space)) 
+        if (Input.GetKeyUp(KeyCode.Space) && !audioSource.isPlaying) 
         { 
             audioSource.PlayOneShot(clip);
         }
